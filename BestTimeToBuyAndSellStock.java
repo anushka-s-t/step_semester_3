11acfc062840c475ac8c9d5b3577a42d60ddb05d
anushka-s-t <at0665@srmist.edu.in>
2026-09-05 16:46:54 +0530

    pp2

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,35 @@
+public class BestTimeToBuyAndSellStock {
+
+    public static int maxProfit(int[] prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            // Update minimum price
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            // Calculate today's profit
+            int profit = prices[i] - minPrice;
+
+            // Update maximum profit
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int result = maxProfit(prices);
+
+        System.out.println(result);
+    }
+}
