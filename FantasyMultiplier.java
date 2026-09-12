bef8c1dec6b7b2b3cb020dc31d5d24d117de4869
Anushka <at0665@srmist.edu.in>
2026-09-12 07:19:14 +0530

    added

--- FantasyMultiplier.java
+++
@@ -1,7 +0,0 @@
-public class FantasyMultiplier {
-    static void applyMultipliers(double[] playerScores, int captainIndex, int viceCaptainIndex) {
-        // Directly modify the values at the specified indices without a loop
-        playerScores[captainIndex] *= 2.0;
-        playerScores[viceCaptainIndex] *= 1.5;
-    }
-}
