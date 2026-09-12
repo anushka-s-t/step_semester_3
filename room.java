f5ab336e0341f0de09490fec45e55ffc01d1679a
Anushka <at0665@srmist.edu.in>
2026-09-12 06:30:48 +0530

    pp

---
+++ room.java
@@ -0,0 +1,22 @@
+class HostelRoom {
+    String roomNo;
+    int occupied;
+}
+
+public class room {
+    public static void main(String[] args) {
+        HostelRoom roomA = new HostelRoom();
+        HostelRoom roomB = new HostelRoom();
+
+        roomA.roomNo = "C-101";
+        roomB.roomNo = "C-102";
+
+        // Increment roomA's occupied field three times
+        roomA.occupied++;
+        roomA.occupied++;
+        roomA.occupied++;
+
+        System.out.println(roomA.roomNo + " occupied: " + roomA.occupied);
+        System.out.println(roomB.roomNo + " occupied: " + roomB.occupied);
+    }
+}
